751dea19030ff45478b1e646337b2949faed5ab5
jujudev7 <penna.julien@gmail.com>
2024-04-20 15:31:48 +0200

    icon Github & Linkedin added

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,5 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Section } from "./Section";
+import { GithubIcon } from "./icons/GithubIcon";
+import Link from "next/link";
+import { LinkedinIcon } from "./icons/LinkedinIcon";
 
 export const Header = () => {
   return (
@@ -8,8 +11,11 @@ export const Header = () => {
         <h1>jujudev7.com</h1>
         <div className="flex-1"></div>
         <div>
-          {/* <Button className="size-6"></Button> */}
-          <Button className="size-6">Test</Button>
+          <Button className="size-6 p-0"></Button>
+          <Link href="https://github.com/jujudev7" target="_blank">
+            <GithubIcon size={32} />
+            <LinkedinIcon size={32} />
+          </Link>
         </div>
       </Section>
     </header>

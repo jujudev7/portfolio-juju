34cc77315a03481d2889faee588910e11d095438
jujudev7 <penna.julien@gmail.com>
2024-05-06 16:00:20 +0200

    strong & flipcard OK

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -13,36 +13,47 @@ export const Hero = () => {
           </i>
         </h1>
       </div>
-      <div className="flex my-10">
+      <div className="flex">
         <div className="flex-[2]">
           <p>Salut 👋 moi c&apos;est Julien,</p>
           <p>
             Je suis un développeur front-end français, spécialisé en{" "}
-            <strong>React.js</strong>. Grâce à mes compétences, je peux intégrer
+            <strong className="strong">React.js</strong>. Grâce à mes compétences, je peux intégrer
             vos maquettes et créer des{" "}
-            <strong>applications interactives et réactives</strong>.
+            <strong className="strong">applications interactives et réactives</strong>.
           </p>
           <p>
-            Doté d&apos;une <strong>forte capacité d&apos;adaptation</strong> et d&apos;un
-            désir constant d&apos;apprentissage, je suis prêt à relever de nouveaux
-            défis !
+            Doté d&apos;une <strong className="strong">forte capacité d&apos;adaptation</strong> et
+            d&apos;un désir constant d&apos;apprentissage, je suis prêt à
+            relever de nouveaux défis !
           </p>
         </div>
         <div className="flex-1 ml-4">
-          <div
-            className="flip-container"
-          >
-            <div className="flipper">
-              <div className="front">
+          <div className="photo-container rounded-xl">
+            <div className="back">
+              <p>
+                Basé en <span className="exergue">région parisienne</span>, j'ai
+                vécu quelque temps à Barcelone.
+              </p>
+              <p>
+                Je suis beaucoup le sport, surtout le{" "}
+                <span className="exergue">foot</span>. Supporter du PSG, bien avant l'arrivée des Qataris.
+              </p>
+              <p>
+                Je fais près de <span className="exergue">2 mètres</span>.
+              </p>
+              <p>
+                J'aime <span className="exergue">l'humour</span>, la raclette, la pâte à tartiner...
+              </p>
+            </div>
+            <div className="front">
+              <div className="image">
                 <Image
                   src="/jujudev7.jpg"
-                  width={600} height={400}
-                  className="w-full h-auto rounded-xl"
+                  width={360}
+                  height={240}
                   alt="portrait Julien Penna"
                 />
-              </div>
-              <div className="back">
-                <Image src="/yo.png" alt="Image 2" width={600} height={400} />
               </div>
             </div>
           </div>

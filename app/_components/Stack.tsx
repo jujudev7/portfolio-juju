6656e6adeb408cf83f7566cba35e9255e047b755
jujudev7 <penna.julien@gmail.com>
2024-06-27 14:51:05 +0200

    logo nextjs in darkmode

--- app/_components/Stack.tsx
+++ app/_components/Stack.tsx
@@ -7,6 +7,7 @@ interface StackItem {
   src: string;
   width: number;
   height: number;
+  className?: string;
 }
 
 const stackItems: StackItem[] = [
@@ -15,48 +16,56 @@ const stackItems: StackItem[] = [
     src: "/logos/react-original-wordmark.svg",
     width: 128,
     height: 128,
+    className: "logo",
   },
   {
     name: "Next.js",
     src: "/logos/nextjs-original-wordmark.svg",
     width: 128,
     height: 128,
+    className: "logo nextjs",
   },
   {
     name: "JavaScript",
     src: "/logos/javascript-original.svg",
     width: 128,
     height: 128,
+    className: "logo",
   },
   {
     name: "TypeScript",
     src: "/logos/typescript-original.svg",
     width: 128,
     height: 128,
+    className: "logo",
   },
   {
     name: "Tailwind",
     src: "/logos/tailwind-original-wordmark.svg",
     width: 128,
     height: 128,
+    className: "logo",
   },
   {
     name: "Bootstrap",
     src: "/logos/bootstrap-original-wordmark.svg",
     width: 128,
     height: 128,
+    className: "logo",
   },
   {
     name: "HTML",
     src: "/logos/html5-original-wordmark.svg",
     width: 128,
     height: 128,
+    className: "logo",
   },
   {
     name: "CSS",
     src: "/logos/css3-original-wordmark.svg",
     width: 128,
     height: 128,
+    className: "logo",
   },
 ];
 
@@ -71,7 +80,7 @@ const Stack: React.FC = () => {
               alt={item.name}
               width={item.width}
               height={item.height}
-              className="logo"
+              className={item.className}
             />
           </div>
         ))}
@@ -82,7 +91,7 @@ const Stack: React.FC = () => {
               alt={item.name}
               width={item.width}
               height={item.height}
-              className="logo"
+              className={item.className}
             />
           </div>
         ))}

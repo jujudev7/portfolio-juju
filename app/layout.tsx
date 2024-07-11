e4cf2e1768dcfecb3943d56192e6fffaabcaee21
jujudev7 <penna.julien@gmail.com>
2024-07-11 22:52:23 +0200

    favicon + head in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,26 +14,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className="h-full bg-background text-foreground">
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <meta name="theme-color" content="#ffffff" />
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>{children}</body>
     </html>
   );

b282b3563670de06d2d5cf605c7496a197a3fda5
jujudev7 <penna.julien@gmail.com>
2024-10-05 21:22:10 +0200

    CORS Headers added

--- app/api/submit-form/route.ts
+++ app/api/submit-form/route.ts
@@ -2,29 +2,18 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-// Middleware CORS
-export function applyCORS(req: Request) {
-  const allowedOrigins = ["https://julienpenna.com"]; // Remplacez par votre domaine
-  const origin = req.headers.get("origin");
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "http://localhost:3000",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
 
-  if (allowedOrigins.includes(origin as string)) {
-    const response = NextResponse.next();
-    response.headers.append("Access-Control-Allow-Origin", origin);
-    response.headers.append("Access-Control-Allow-Methods", "POST");
-    response.headers.append("Access-Control-Allow-Headers", "Content-Type");
-    return response;
-  }
-
-  return NextResponse.next();
+export async function OPTIONS() {
+  // Répondre aux requêtes OPTIONS
+  return NextResponse.json({}, { status: 200, headers: corsHeaders });
 }
 
 export async function POST(request: Request) {
-  // Appliquer le middleware CORS
-  const corsResponse = applyCORS(request);
-  if (corsResponse) {
-    return corsResponse; // Retourne la réponse modifiée si CORS est appliqué
-  }
-
   const body = await request.json();
 
   // Extraire les données du formulaire
@@ -32,31 +21,32 @@ export async function POST(request: Request) {
 
   // Configurer Nodemailer
   const transporter = nodemailer.createTransport({
-    service: "gmail", // ou un autre service d'e-mail
+    service: "gmail",
     auth: {
-      user: process.env.GMAIL_USER, // Ton adresse e-mail
-      pass: process.env.GMAIL_PASS, // Ton mot de passe d'application
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
     },
   });
 
-  // Définir le contenu de l'e-mail
   const mailOptions = {
-    from: process.env.GMAIL_USER, // Adresse de l'expéditeur
-    to: process.env.GMAIL_USER, // Adresse de l'expéditeur ou autre adresse
-    subject: subject, // Sujet de l'e-mail
-    text: `Nom: ${name}\nEmail: ${email}\n\nMessage:\n${message}`, // Contenu de l'e-mail
+    from: process.env.GMAIL_USER,
+    to: process.env.GMAIL_USER,
+    subject: subject,
+    text: `Nom: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
   };
 
-  // Envoyer l'e-mail
   try {
     await transporter.sendMail(mailOptions);
     console.log("E-mail envoyé avec succès !");
-    return NextResponse.json({ message: "Formulaire soumis avec succès" });
+    return NextResponse.json(
+      { message: "Formulaire soumis avec succès" },
+      { headers: corsHeaders }
+    );
   } catch (error) {
     console.error("Erreur lors de l'envoi de l'e-mail :", error);
     return NextResponse.json(
       { message: "Erreur lors de l'envoi de l'e-mail" },
-      { status: 500 }
+      { status: 500, headers: corsHeaders }
     );
   }
 }

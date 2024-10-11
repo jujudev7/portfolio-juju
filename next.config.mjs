6658c92473e7855cbb83cc80cf852dad9c2498a6
jujudev7 <penna.julien@gmail.com>
2024-10-12 00:00:52 +0200

    img unoptimized for export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // Désactive l'optimisation d'image pour le mode exporté
+  },
 
   async headers() {
     return [
